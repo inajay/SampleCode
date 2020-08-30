9cfc46c9ab173a726379ca4d5e690b611ec3d237
inajay <59800459+inajay@users.noreply.github.com>
2020-08-30 11:02:26 +0530

    Add files via upload

---
+++ WindowsFormsAppTest/WindowsFormsAppTest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppTest
+{
+    public partial class Form1 : Form
+    {
+        private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(2, 2);
+        CancellationTokenSource cancelSource = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickMe_Click(object sender, EventArgs e)
+        {
+            cancelSource = new CancellationTokenSource();
+            var dueTime = TimeSpan.FromSeconds(10);
+            var interval = TimeSpan.FromSeconds(15);
+
+            Debug.WriteLine($"{DateTime.Now.ToString() } Task started ");
+            await PeriodicRunGetLogs(FetchDataAtGivenInterval, dueTime, interval, cancelSource.Token, "Ajay", "Tripathi");
+        }
+
+        private async Task PeriodicRunGetLogs(Action<string, string> FetchDataAction, TimeSpan dueTime, TimeSpan interval,
+          CancellationToken token, string x, string y)
+        {
+
+            await Task.Delay(dueTime, token);
+            await _semaphoreSlim.WaitAsync();
+            while (!token.IsCancellationRequested)
+            {
+                FetchDataAction?.Invoke(x, y);
+                await Task.Delay(interval, token);
+            }
+             _semaphoreSlim.Release();
+        }
+
+        private void FetchDataAtGivenInterval(string a, string b)
+        {
+            string z = a + b;
+            Debug.WriteLine($"{DateTime.Now.ToString()} {z} ");
+
+        }
+    }
+}
---
+++ WindowsFormsAppTest/WindowsFormsAppTest/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppTest
+{
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+    public class Person : Student
+    {
+        public Person(string name): base(name)
+        {
+
+        }
+    }
+
+   public class Student
+    {
+        public Student(string name)
+        {
+            
+        }
+
+        public int RollNo { get; set; }
+
+        public String  NameOfStudent { get; set; }
+
+        public string GetAddress(string name)
+        {
+            return name + "City Name";
+        }
+    }
+}
